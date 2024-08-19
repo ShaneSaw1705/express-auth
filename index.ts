ec3e260e4be5e0df7e4f7468ec1a56cca957b5e2
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-08-19 15:08:55 +1200

    added library ready for implementing auth using express

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import express from 'express'
+import express from "express"
 
 const app = express()
 
 app.get('/ping', (req, res) => {
-  res.send('pong')
+  return res.json('pong').status(200)
 })
 
+app.set("trust proxy", true)
+
 app.listen(8080, () => {
-  console.log('Listening on port 8080')
+  console.log('server running on port 8080')
 })
